6802f29c67b2ce657b766fab0a82d5ee065d627b
Lewis <lewis.frampton@gmail.com>
2020-11-25 01:14:11 +0000

    Joint 4 is wack, will fix tomorrow but now publishing and seems to be working

--- catkin_ws/src/ivr_assignment/src/image1.py
+++ catkin_ws/src/ivr_assignment/src/image1.py
@@ -27,6 +27,11 @@ class image_converter:
     self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
     self.robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
 
+
+    self.robot_joint2Vision_pub = rospy.Publisher("/robot/joints2Vision", Float64, queue_size=10)
+    self.robot_joint3Vision_pub = rospy.Publisher("/robot/joints3Vision", Float64, queue_size=10)
+    self.robot_joint4Vision_pub = rospy.Publisher("/robot/joints4Vision", Float64, queue_size=10)
+
     # colour_closest in format [Closest colour in YZ, Closest colour in XZ]
     # Used in case we can't see target colour, use the closest objects coordinates instead
     # Initialise to centre points just to clear any unforeseen errors.
@@ -40,6 +45,10 @@ class image_converter:
     self.joint2=Float64()
     self.joint3=Float64()
     self.joint4=Float64()
+
+    self.calcJoint2=Float64()
+    self.calcJoint3=Float64()
+    self.calcJoint4=Float64()
 
     self.pixToMet = None
     self.looped = False
@@ -60,101 +69,70 @@ class image_converter:
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
 
-
-
+    # get colour masks
     # colourMasks in format [redMask(YZ), redMask(XZ)]
-    self.redMask   = [self.get_red_mask(self.cv_image1), self.get_red_mask(self.cv_image2)]
-    self.blueMask   = [self.get_blue_mask(self.cv_image1), self.get_blue_mask(self.cv_image2)]
-    self.yellowMask = [self.get_yellow_mask(self.cv_image1), self.get_yellow_mask(self.cv_image2)]
-    self.greenMask  = [self.get_green_mask(self.cv_image1), self.get_green_mask(self.cv_image2)]
-    self.orangeMask = [self.get_orange_mask(self.cv_image1), self.get_orange_mask(self.cv_image2)]
+    redMask   = [self.get_red_mask(self.cv_image1), self.get_red_mask(self.cv_image2)]
+    blueMask   = [self.get_blue_mask(self.cv_image1), self.get_blue_mask(self.cv_image2)]
+    yellowMask = [self.get_yellow_mask(self.cv_image1), self.get_yellow_mask(self.cv_image2)]
+    greenMask  = [self.get_green_mask(self.cv_image1), self.get_green_mask(self.cv_image2)]
+    orangeMask = [self.get_orange_mask(self.cv_image1), self.get_orange_mask(self.cv_image2)]
 
     # get the visibility of each colour. colVis = [is seen in YZ, is seen in XZ]
-    self.redVis = self.canSee(self.redMask)
-    self.blueVis = self.canSee(self.blueMask)
-    self.yellowVis = self.canSee(self.yellowMask)
-    self.greenVis = self.canSee(self.greenMask)
-    self.orangeVis = self.canSee(self.orangeMask)
+    redVis = self.canSee(redMask)
+    blueVis = self.canSee(blueMask)
+    yellowVis = self.canSee(yellowMask)
+    greenVis = self.canSee(greenMask)
+    orangeVis = self.canSee(orangeMask)
 
-    self.beans = [0,0,0]
+    visibility = [yellowVis, blueVis, greenVis, redVis, orangeVis]
+    print("!: " + str(visibility[0][0]))
+    redPixCentres = self.get_pixel_centre(redMask, redVis, self.redClosest)
+    bluePixCentres = self.get_pixel_centre(blueMask, blueVis, self.blueClosest)
+    yellowPixCentres = self.get_pixel_centre(yellowMask, yellowVis, self.YellowClosest)
+    greenPixCentres = self.get_pixel_centre(greenMask, greenVis, self.greenClosest)
 
-    self.redPixCentres = self.get_pixel_centre(self.redMask, self.redVis, self.redClosest)
-    self.bluePixCentres = self.get_pixel_centre(self.blueMask, self.blueVis, self.blueClosest)
-    self.yellowPixCentres = self.get_pixel_centre(self.yellowMask, self.yellowVis, self.YellowClosest)
-    self.greenPixCentres = self.get_pixel_centre(self.greenMask, self.greenVis, self.greenClosest)
-    orangePixCentresYZ = self.orangeObjectsPixelPos(self.orangeMask[0])
-    orangePixCentresXZ = self.orangeObjectsPixelPos(self.orangeMask[1])
-    self.targetPixCentres = np.array([orangePixCentresYZ[0], orangePixCentresXZ[0]])
-    self.cubePixCentres = np.array([orangePixCentresYZ[1], orangePixCentresXZ[1]])
+    orangePixCentresYZ = self.orangeObjectsPixelPos(orangeMask[0], 0)
+    orangePixCentresXZ = self.orangeObjectsPixelPos(orangeMask[1], 1)
 
-    """
-    print("redPixCentres: " + str(self.redPixCentres))
-    print("bluePixCentres: " + str(self.bluePixCentres))
-    print("yellowPixCentres: " + str(self.yellowPixCentres))
-    print("greenPixCentres: " + str(self.greenPixCentres))
-    print("targetPixCentres: " + str(self.targetPixCentres))
-    print("cubePixCentres: " + str(self.cubePixCentres))
-    print("----")
-    """
+    targetPixCentres = np.array([orangePixCentresYZ[0], orangePixCentresXZ[0]])
+    cubePixCentres = np.array([orangePixCentresYZ[1], orangePixCentresXZ[1]])
+
+    pixelPoints = [yellowPixCentres, bluePixCentres, greenPixCentres, redPixCentres, targetPixCentres, cubePixCentres]
 
     # get pixel to meter ratio only once, do this before any joints have moved.
     if not self.looped:
-      self.pixToMet = self.pixel2Meter(self.yellowPixCentres[0], self.redPixCentres[0])
+      self.pixToMet = self.pixel2Meter(yellowPixCentres[0], redPixCentres[0])
       self.looped = True
 
     # Get 2D object locations in meters on the YZ and the XZ
-    self.red2D  = np.array([self.pixToMet * self.redPixCentres[0], self.pixToMet * self.redPixCentres[1]])
-    self.blue2D = np.array([self.pixToMet * self.bluePixCentres[0], self.pixToMet * self.bluePixCentres[1]])
-    self.yellow2D = np.array([self.pixToMet * self.yellowPixCentres[0], self.pixToMet * self.yellowPixCentres[1]])
-    self.green2D = np.array([self.pixToMet * self.greenPixCentres[0], self.pixToMet * self.greenPixCentres[1]])
-    self.target2D = np.array([self.pixToMet * self.targetPixCentres[0], self.pixToMet * self.targetPixCentres[1]])
-    self.cube2D = np.array([self.pixToMet * self.cubePixCentres[0], self.pixToMet * self.cubePixCentres[1]])
+    red2D  = np.array([self.pixToMet * redPixCentres[0], self.pixToMet * redPixCentres[1]])
+    blue2D = np.array([self.pixToMet * bluePixCentres[0], self.pixToMet * bluePixCentres[1]])
+    yellow2D = np.array([self.pixToMet * yellowPixCentres[0], self.pixToMet * yellowPixCentres[1]])
+    green2D = np.array([self.pixToMet * greenPixCentres[0], self.pixToMet * greenPixCentres[1]])
+    target2D = np.array([self.pixToMet * targetPixCentres[0], self.pixToMet * targetPixCentres[1]])
+    cube2D = np.array([self.pixToMet * cubePixCentres[0], self.pixToMet * cubePixCentres[1]])
 
-    """
-    print("red2D: " + str(self.red2D))
-    print("blue2D: " + str(self.blue2D))
-    print("yellow2D: " + str(self.yellow2D))
-    print("green2D: " + str(self.green2D))
-    print("target2D: " + str(self.target2D))
-    print("cube2D: " + str(self.cube2D))
-    print("---")
-    """
+    points2D = [red2D, blue2D, yellow2D, green2D, target2D, cube2D]
+
+
     # get 3D coordinates in meters for each object
-    self.red3D = self.get3Dim(self.red2D)
-    self.blue3D = self.get3Dim(self.blue2D)
-    self.yellow3D = self.get3Dim(self.yellow2D)
-    self.green3D = self.get3Dim(self.green2D)
-    self.target3D = self.get3Dim(self.target2D)
-    self.cube3D = self.get3Dim(self.cube2D)
+    red3D = self.get3Dim(red2D)
+    blue3D = self.get3Dim(blue2D)
+    yellow3D = self.get3Dim(yellow2D)
+    green3D = self.get3Dim(green2D)
+    target3D = self.get3Dim(target2D)
+    cube3D = self.get3Dim(cube2D)
 
 
-    print("red3D: " + str(self.red3D))
-    print("blue3D: " + str(self.blue3D))
-    print("yellow3D: " + str(self.yellow3D))
-    print("green3D: " + str(self.green3D))
-    print("target3D: " + str(self.target3D))
-    print("cube3D: " + str(self.cube3D))
-    print("---")
 
+    calcJoints = self.calcJointAngles([yellow3D, blue3D, green3D, red3D])
 
-    """
-    # uncomment to show image showing the centre of each point in the YZ perspective
-    test = np.zeros((self.cv_image1.shape[0], self.cv_image1.shape[1], 3), np.uint8)
+    self.calcJoint2 = calcJoints[0]
+    self.calcJoint3 = calcJoints[1]
+    self.calcJoint4 = calcJoints[2]
 
-    test[self.redPixCentres[0][1]][self.redPixCentres[0][0]] = (255,255,255)
-    test[self.bluePixCentres[0][1]][self.bluePixCentres[0][0]] = (255,255,255)
-    test[self.yellowPixCentres[0][1]][self.yellowPixCentres[0][0]] = (255,255,255)
-    test[self.greenPixCentres[0][1]][self.greenPixCentres[0][0]] = (255,255,255)
-    test[self.targetPixCentres[0][1]][self.targetPixCentres[0][0]] = (255,255,255)
-    test[self.cubePixCentres[0][1]][self.cubePixCentres[0][0]] = (255,255,255)
-    kernal = np.ones((5,5), np.uint8)
-    x = cv2.imshow("pls", cv2.dilate(test, kernal, iterations=3))
-    """
+    self.setClosestPoints(points2D, visibility)
 
-    print("j2 calc = " + str(self.joint2))
-
-
-    self.calcJointAngles([self.yellow3D, self.blue3D, self.green3D, self.red3D])
     # Update joint angles
     joints = self.jointMovement()
     self.joint2 = joints[0]
@@ -162,25 +140,24 @@ class image_converter:
     self.joint4 = joints[2]
     print("Joints:   J2:" + str(np.round(10000 * joints[0]) /10000) + ",  J3:" + str(np.round(10000 * joints[1]) /10000) + ",  J4:" + str(np.round(10000 * joints[2]) /10000))
 
-    #print(len(cont))
-    imcopy = cv2.cvtColor(self.orangeMask[0].copy(), cv2.COLOR_GRAY2BGR)
-    #cv2.drawContours(imcopy, cont, -1, (0,255,75), 3)
     im1 = cv2.imshow('yzImage', self.cv_image1)
     im2 = cv2.imshow('xz,Image', self.cv_image2)
-    #im2 = cv2.imshow('xzImage', self.cv_image2)
-    #w1 = cv2.imshow('cimg', imcopy)
-    #w2 = cv2.imshow('xz', self.orangeMask[1])
+
     cv2.waitKey(1)
     # Publish the results
     try:
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
-      self.robot_joint2_pub.publish(self.joint2);
-      self.robot_joint3_pub.publish(self.joint3);
-      self.robot_joint4_pub.publish(self.joint4);
+      self.robot_joint2_pub.publish(self.joint2)
+      self.robot_joint3_pub.publish(self.joint3)
+      self.robot_joint4_pub.publish(self.joint4)
+
+      self.robot_joint2Vision_pub.publish(self.calcJoint2)
+      self.robot_joint3Vision_pub.publish(self.calcJoint3)
+      self.robot_joint4Vision_pub.publish(self.calcJoint4)
     except CvBridgeError as e:
       print(e)
 
-  def orangeObjectsPixelPos(self, image):
+  def orangeObjectsPixelPos(self, image, index):
     targetPixelPos = np.array([-1, -1])
     cubePixelPos = np.array([-1, -1])
     targetContours = None
@@ -193,7 +170,7 @@ class image_converter:
     # think it can actually happen while running. If I have time I'll come back to this later
     if(len(cont) == 0):
       print("this really shouldn't happen")
-      return np.array([[0,0],[0,0]])
+      return np.array([self.sphereClosest, self.cubeClosest])
 
     # if objects are overlapping in view we just take the centre of both. This skew the results this is a rare
     # occurrence and if it does happen, it's usually for a tiny amount of time.
@@ -211,10 +188,12 @@ class image_converter:
         targetContours = cont[1]
         cubeContours = cont[0]
 
-    # this REALLY shouldn't happen but if it does then just return the last valid position
+    # this rarely happens but if it does then just return the last valid position#
+    # only potential times this happens is when a link passes in front of a the orange objects
     elif len(cont) > 2:
-      print("something's broken ! :D")
-      return np.array([[0,0],[0,0]])
+      print("more than two contours")
+      print("!!!")
+      return np.array([self.sphereClosest[index], self.cubeClosest[index]])
 
     # get the moments of the contours
     targM = cv2.moments(targetContours)
@@ -345,9 +324,9 @@ class image_converter:
     #j2 = self.xAngle(green - blue) - j1
 
     j2 = - (np.arctan2(blue[1] - green[1], blue[2] - green[2]) - J1X)
-    #temp = j2 < 0
-    #j2 = np.abs(j2) + 0.1
-    #if(temp): j2 * -1
+    temp = j2 < 0
+    j2 = np.abs(j2) - 0.1
+    if(temp): j2 = j2 * -1
     #print("J3 testing: J3 =" + str((np.arctan2(blue[0] - green[0], blue[2] - green[2]))) + ", J1Y: "+ str(J1Y))
     print("Green Testing: " + str(green) + "  --- j3: " +  str((-1 * np.abs(np.arctan2(blue[0] - green[0], blue[2] - green[2]))) + J1Y));
     j3 =  (-1 * np.abs(np.arctan2(blue[0] - green[0], blue[2] - green[2]))) + J1Y
@@ -362,7 +341,11 @@ class image_converter:
 
     j4 = np.abs((np.arctan2(green[1] - red[1], green[2] - red[2]))) - J1X - j2
 
+
+
+
     print("calc:     J2:" + str(np.round(1000 * j2) /1000) + ",  J3:" + str(np.round(1000 * j3) /1000) + ",  J4:" + str(np.round(1000 * j4) /1000))
+    return [j2, j3, j4]
 
   def pixel2Meter(self, circle1, circle2):
     return 2.5 / np.sqrt(np.sum((circle1 - circle2)**2))
@@ -375,6 +358,42 @@ class image_converter:
 
   def zAngle(self, vector):
     return math.acos(np.dot([0,0], [vector[0], vector[1]]));
+
+  def getClosestPoint(self, points, ownPoint, index):
+    closestPoint = None
+    MinDist = 1000000;
+    for point in points:
+      Dist = self.sqrdist(point[index], ownPoint)
+
+      if Dist < MinDist and Dist != 0:
+        closestPoint = point[index]
+        MinDist = Dist
+    return closestPoint
+
+  def setClosestPoints(self, points, visibility):
+    # if we can see it, update it!
+    # if you're reading this, I wrote this at like 1:30am and yeah I know it's horrible but I do not care at this point!
+    # It's all being abstracted away in a function to be forgotten about anyways
+    if visibility[0][0]: self.YellowClosest[0] = self.getClosestPoint(points, points[0][0], 0)
+    if(visibility[0][1]): self.YellowClosest[1] = self.getClosestPoint(points, points[0][1], 1)
+
+    if(visibility[1][0]): self.blueClosest[0] = self.getClosestPoint(points, points[1][0], 0)
+    if(visibility[1][1]): self.blueClosest[1] = self.getClosestPoint(points, points[1][1], 1)
+
+    if(visibility[2][0]): self.greenClosest[0] = self.getClosestPoint(points, points[2][0], 0)
+    if(visibility[2][1]): self.greenClosest[1] = self.getClosestPoint(points, points[2][1], 1)
+
+    if(visibility[3][0]): self.redClosest[0] = self.getClosestPoint(points, points[3][0], 0)
+    if(visibility[3][1]): self.redClosest[1] = self.getClosestPoint(points, points[3][1], 1)
+
+    if(visibility[4][0]): self.sphereClosest[0] = self.getClosestPoint(points, points[4][0], 0)
+    if(visibility[4][1]): self.sphereClosest[1] = self.getClosestPoint(points, points[4][1], 1)
+
+    if(visibility[4][0]): self.cubeClosest[0] = self.getClosestPoint(points, points[5][0], 0)
+    if(visibility[4][1]): self.cubeClosest[1] = self.getClosestPoint(points, points[5][1], 1)
+
+  def sqrdist(self, p1, p2):
+    return np.linalg.norm(p1 - p2)
 
 # call the class
 def main(args):
